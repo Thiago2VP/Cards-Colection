3c0b48b4c36ae7dde4a46ebf2b521a12c14a4a7a
Thiago2VP <thiago_vasconcelosvp@outlook.com>
2022-07-28 11:24:00 -0300

    nao recarrega a pagina inteira e mundanca na imagem de fundo

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if(el.parentNode.getAttribute('href')!=='index.html' && (tag === 'img' || tag === 'h2' || tag === 'a')) {
+        e.preventDefault();
+        if(tag === 'img') carregaPagina(el.parentNode);
+    }
+});
+    
+async function carregaPagina(el) {
+    try {
+        const href = el.getAttribute('href');
+        const response = await fetch(href);
+
+        if(response.status !== 200) throw new Error('ERRO 404 NOSSO');
+
+        const html = await response.text();
+        carregaResultado(html);
+
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+function carregaResultado(response) {
+    const ressultado = document.querySelector('.content');
+    ressultado.innerHTML = response;
+}
